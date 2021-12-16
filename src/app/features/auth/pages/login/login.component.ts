072c8183f1fe367df50befd3fc0f9f60ccf46e8b
MatiasProietti <95661104+MatiasProietti@users.noreply.github.com>
2021-12-16 19:03:15 -0300

    refactor(auth): refactored login to use authFormComponent

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FieldGroup } from "@app/shared/models/fieldGroup";
+import { FormValue } from "@app/shared/models/formValue";
+import { Validators } from "@app/shared/validators/validators";
 
 @Component({
   selector: "app-auth-login",
@@ -7,16 +9,28 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent {
-  public loginForm: FormGroup;
   public revealPassword = false;
-  constructor(private formBuilder: FormBuilder) {
-    this.loginForm = this.formBuilder.group({
-      username: ["", Validators.required],
-      password: ["", Validators.required],
-    });
-  }
+  public fieldGroup = new FieldGroup([
+    {
+      name: "username",
+      displayName: "Username",
+      type: "text",
+      icon: "account_box",
+      autocomplete: "username",
+      validators: [Validators.required],
+    },
+    {
+      name: "password",
+      displayName: "Password",
+      type: "password",
+      icon: "vpn_key",
+      autocomplete: "current-password",
+      validators: [Validators.required],
+    },
+  ]);
+  constructor() {}
 
-  toggleRevealPassword(): void {
-    this.revealPassword = !this.revealPassword;
+  public onSubmit($event: FormValue): void {
+    console.log($event);
   }
 }
