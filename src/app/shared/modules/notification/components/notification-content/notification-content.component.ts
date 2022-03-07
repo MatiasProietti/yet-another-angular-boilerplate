11f856acab9f54aac48d5c3a4c79c9c193cb92d0
MatiasProietti <95661104+MatiasProietti@users.noreply.github.com>
2022-03-07 12:25:50 -0300

    refactor(notification): replaced ng-deep with ViewEncapsulation.None

--- src/app/shared/modules/notification/components/notification-content/notification-content.component.ts
+++ src/app/shared/modules/notification/components/notification-content/notification-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, EventEmitter, ViewEncapsulation, HostBinding } from "@angular/core";
 import { NotificationType } from "../../constants/notification-type";
 import { NotificationConfig } from "../../interfaces/notification-config";
 
@@ -6,8 +6,10 @@ import { NotificationConfig } from "../../interfaces/notification-config";
   selector: "app-notification-content",
   templateUrl: "./notification-content.component.html",
   styleUrls: ["./notification-content.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NotificationContentComponent implements NotificationConfig {
+  @HostBinding() class = "app-notification-content";
   @Input() type: NotificationType = NotificationType.ERROR;
   @Input() title = "";
   @Input() text = "";
