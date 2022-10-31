1e93ae098b7ffa77f7ad371976456f672f7195c9
MatiasProietti <95661104+MatiasProietti@users.noreply.github.com>
2022-10-31 14:02:46 -0300

    refactor(shared): removed constants file and moved error text to the service

--- src/app/shared/services/mat-error.service.ts
+++ src/app/shared/services/mat-error.service.ts
@@ -1,21 +1,36 @@
-import { Injectable } from "@angular/core";
-import { AbstractControl } from "@angular/forms";
-import { VALIDATORS_ERRORS } from "../constants/validators-errors";
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class MatErrorService {
-  constructor() {}
+  /**
+   * @description Returns the error message for the first error found in the ctrl.
+   * @remarks If no error is found then it returns an empty string
+   * @param {(AbstractControl | null)} ctrl
+   * @param {string} displayName
+   * @returns {*}  {string}
+   */
+  public getErrorMessage(ctrl: AbstractControl | null, displayName: string): string {
+    if (!ctrl) return '';
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public getErrorMessage(ctrl: AbstractControl | null, _displayName: string): string {
-    if (!ctrl) return "";
-    const error = VALIDATORS_ERRORS.find((error) => ctrl.hasError(error.name));
+    let errorText = '';
 
-    if (!error) return "";
+    // This could be moved to a constants file for easier visualization but can't find a way that doesn't add complexity due to the variables in the errorText.
 
-    // this method can be improved. Using eval doesn't seem very intuitive.
-    return eval(error.text) as string;
+    if (ctrl.hasError('controlsMatch')) {
+      errorText = `${displayName} must match`;
+    } else if (ctrl.hasError('required')) {
+      errorText = `${displayName} is required`;
+    } else if (ctrl.hasError('minlength')) {
+      errorText = `${displayName} must contain at least ${ctrl.getError('minlength').requiredLength} characters`;
+    } else if (ctrl.hasError('maxlength')) {
+      errorText = `${displayName} must contain below ${ctrl.getError('maxlength').requiredLength} characters`;
+    } else if (ctrl.hasError('email')) {
+      errorText = `${displayName} must be a valid email address`;
+    }
+
+    return errorText;
   }
 }
