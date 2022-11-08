18506125fabe7be0eeda68493e33d5860ced0b08
MatiasProietti <95661104+MatiasProietti@users.noreply.github.com>
2022-11-08 20:31:14 -0300

    chore(shared): removed removeElement()
    It was an experimental implementation, wasn't mean to be comitted.

--- src/app/shared/utils/arr.ts
+++ src/app/shared/utils/arr.ts
@@ -1,13 +1,6 @@
-import { Random } from "./random";
+import { Random } from './random';
 
 export class Arr {
-  static removeElement<T>(array: T[], element: T): T[] {
-    return array.splice(
-      array.findIndex((arrElement) => arrElement === element),
-      1
-    );
-  }
-
   static randomElement<T>(array: T[]): T {
     const randomIndex = Random.integer(0, array.length - 1);
     return array[randomIndex];
